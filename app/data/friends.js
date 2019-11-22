056ab6230399edee49052886fdacfb02256456e4
Monica Bongue <muddyforkfarm@gmail.com>
2019-11-22 10:18:25 -0500

    draft2

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [{
+        name: 'Chris Zylka- spider man',
+        photo: 'https://www.imdb.com/name/nm3024712/mediaviewer/rm3745712640',
+        scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
+    },
+    {
+        name: 'LeBron James',
+        photo: 'https://www.imdb.com/name/nm1429908/mediaviewer/rm21207552',
+        scores: [5, 1, 1, 1, 2, 2, 3, 3, 4, 4],
+    },
+    {
+        name: 'Margaret Hamilton',
+        photo: 'https://www.imdb.com/name/nm0002121/mediaviewer/rm2805713920',
+        scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5],
+    },
+    {
+        name: 'Halle Berry',
+        photo: 'https://www.imdb.com/name/nm0000932/mediaviewer/rm2613874432',
+        scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
+    },
+    {
+        name: 'Annie Oakley',
+        photo: 'https://en.wikipedia.org/wiki/Annie_Oakley#/media/File:Annie_Oakley_by_Bakers_Art_Gallery_c1880s - crop.jpg ',
+        scores: [1, 1, 1, 2, 2, 2, 3, 3, 3, 5],
+    },
+    {
+        name: 'Sarah Jessica Parker',
+        photo: 'https://en.wikipedia.org/wiki/Sarah_Jessica_Parker#/media/File:Sarah_Jessica_Parker_at_Miami_Rhapsody_30th_Anniversary_Celebration.jpg',
+        scores: [4, 4, 3, 3, 2, 2, 1, 1, 5, 5],
+    },
+    {
+        name: 'Jeffrey Dahmer',
+        photo: '.wikipedia.org/wiki/Jeffrey_Dahmer#/media/File:Jeffrey_Dahmer_Milwaukee_Police_1991_mugshot.jpg',
+        scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 1],
+    },
+    {
+        name: 'Stephanie Hightower',
+        photo: 'https://en.wikipedia.org/wiki/Stephanie_Hightower#/media/File:Hightower.jpg',
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1, ],
+    },
+    {
+        name: 'Tom Crazy',
+        photo: '',
+        scores: [3, 3, 4, 4, 5, 5, 2, 2, 2, 1]
+    },
+    {
+        name: 'Jim Rich Billionare',
+        photo: '',
+        scores: [3, 3, 4, 4, 5, 5, 2, 2, 2, 1]
+    },
+
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,28 @@
+// 2. Your`server.js`file should require the basic npm packages we
+// 've used in class:`express` and`path`.
+
+// Dependencies
+var bodyParser = require("body-parser");
+var express = require("express");
+var path = require("path");
+var app = express();
+
+
+// Set our port to 8080
+var PORT = process.env.PORT || 8080
+//is it false or is it true- what does this mean?
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.text());
+// app.use(bodyParser.json({type: "application/vnd.api + json}));
+
+app.use(express.static("app/public"));
+
+// require("./app/routing/apiRoute.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+    console.log("App lstening on PORT: " + PORT)
+});
