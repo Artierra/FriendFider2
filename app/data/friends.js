58703e08e095373f3808a37d2fb21481caf82ef1
Monica Bongue <muddyforkfarm@gmail.com>
2019-12-04 13:08:23 -0500

    changed images from link to images in folder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,41 @@
 var friends = [{
         name: 'Chris Zylka- spider man',
-        photo: 'https://www.imdb.com/name/nm3024712/mediaviewer/rm3745712640',
+        photo: './assets/images/ChrisZilka.jpg',
         scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
     },
     {
         name: 'LeBron James',
-        photo: 'https://www.imdb.com/name/nm1429908/mediaviewer/rm21207552',
+        photo: './assets/imagesLeBron James.jpg',
         scores: [5, 1, 1, 1, 2, 2, 3, 3, 4, 4],
     },
     {
         name: 'Margaret Hamilton',
-        photo: 'https://www.imdb.com/name/nm0002121/mediaviewer/rm2805713920',
+        photo: './assets/images/Margaret Hamilton.jpg',
         scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5],
     },
     {
         name: 'Halle Berry',
-        photo: 'https://www.imdb.com/name/nm0000932/mediaviewer/rm2613874432',
+        photo: './assets/images/Halle Berry.jpg',
         scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
     },
     {
         name: 'Annie Oakley',
-        photo: 'https://en.wikipedia.org/wiki/Annie_Oakley#/media/File:Annie_Oakley_by_Bakers_Art_Gallery_c1880s - crop.jpg ',
+        photo: './assets/images/AnnieOakley.jpg ',
         scores: [1, 1, 1, 2, 2, 2, 3, 3, 3, 5],
     },
     {
         name: 'Sarah Jessica Parker',
-        photo: 'https://en.wikipedia.org/wiki/Sarah_Jessica_Parker#/media/File:Sarah_Jessica_Parker_at_Miami_Rhapsody_30th_Anniversary_Celebration.jpg',
+        photo: './assets/images/SarahJessicaParker.jpg',
         scores: [4, 4, 3, 3, 2, 2, 1, 1, 5, 5],
     },
     {
         name: 'Jeffrey Dahmer',
-        photo: 'https://en.wikipedia.org/wiki/Jeffrey_Dahmer#/media/File:Jeffrey_Dahmer_Milwaukee_Police_1991_mugshot.jpg',
+        photo: './assets/images/JeffreyDanmer.jpg',
         scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 1],
     },
     {
         name: 'Stephanie Hightower',
-        photo: 'https://en.wikipedia.org/wiki/Stephanie_Hightower#/media/File:Hightower.jpg',
+        photo: './assets/images/Stephanie Higtower.jpg',
         scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1, ],
     }
 
