c45b04876e232bd8354f7049637b3a3ce94b33d3
Monica Bongue <muddyforkfarm@gmail.com>
2019-12-04 16:24:00 -0500

    photos working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,42 @@
 var friends = [{
         name: 'Chris Zylka- spider man',
-        photo: './assets/images/ChrisZilka.jpg',
-        scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
+        photo: '/assets/images/ChrisZylka.jpg',
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     },
     {
         name: 'LeBron James',
-        photo: './assets/imagesLeBron James.jpg',
+        photo: '/assets/images/LeBron James.jpg',
         scores: [5, 1, 1, 1, 2, 2, 3, 3, 4, 4],
     },
     {
         name: 'Margaret Hamilton',
-        photo: './assets/images/Margaret Hamilton.jpg',
+        photo: '/assets/images/Margaret Hamilton.jpg',
         scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5],
     },
+    // 5, 1, 4, 3, 2, 3, 5, 1, 2, 1
     {
         name: 'Halle Berry',
-        photo: './assets/images/Halle Berry.jpg',
+        photo: '/assets/images/Halle Berry.jpg',
         scores: [5, 1, 4, 3, 2, 3, 5, 1, 2, 1],
     },
     {
         name: 'Annie Oakley',
-        photo: './assets/images/AnnieOakley.jpg ',
-        scores: [1, 1, 1, 2, 2, 2, 3, 3, 3, 5],
+        photo: '/assets/images/Annie Oakley.jpg',
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     },
     {
         name: 'Sarah Jessica Parker',
-        photo: './assets/images/SarahJessicaParker.jpg',
-        scores: [4, 4, 3, 3, 2, 2, 1, 1, 5, 5],
+        photo: '/assets/images/SarahJessicaParker.jpg',
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     },
     {
         name: 'Jeffrey Dahmer',
-        photo: './assets/images/JeffreyDanmer.jpg',
+        photo: '/assets/images/JeffreyDahmer.jpg',
         scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 1],
     },
     {
         name: 'Stephanie Hightower',
-        photo: './assets/images/Stephanie Higtower.jpg',
+        photo: '/assets/images/Stephanie Hightower.jpg',
         scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1, ],
     }
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -27,7 +27,7 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
-    var path = require("path");
+
 
 
 
